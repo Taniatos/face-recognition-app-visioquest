3655188ebf5ac5dc98ec5b9e0884993e53deb8e2
Tetiana Korchynska <tanakorchynska@gmail.com>
2025-08-18 10:40:33 -0400

    Update InputForm.js

--- frontend/src/components/InputForm/InputForm.js
+++ frontend/src/components/InputForm/InputForm.js
@@ -1,24 +1,20 @@
 import React from "react";
 import "./InputForm.css"; 
 
-
 const InputForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div id="cover" className="input-form-container">
-    
       <form
         method="get"
         action=""
         className="input-box"
         onSubmit={onButtonSubmit} 
       >
-        
         <div className="tb center searchline">
-          
           <div className="td">
             <input
               type="text"
-              placeholder="Search"
+              placeholder="Paste an image URL to detect faces"
               required
               className="f4 pa2 w-70 center" 
               onChange={onInputChange}
@@ -27,11 +23,9 @@ const InputForm = ({ onInputChange, onButtonSubmit }) => {
           <div className="td" id="s-cover">
             <button
               type="submit"
-              className="input-button center grow f4 link ph3 pv2 dib br1 black"
-              id="btnDetect"
+              className="input-button center grow"
             >
-              <div id="s-circle"></div>
-              <span></span>
+              <i className="fas fa-search"></i>
             </button>
           </div>
         </div>
